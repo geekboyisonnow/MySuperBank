fbf0026ef675462484cf7f80c42471662a1c58cc
geekboyisonnow <geekboyisonnow@github.com>
2021-04-28 17:36:25 -0400

    Finished Basic Bank Console App.  Future plans to Create a new Banking Web App for Savings Goals.

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -8,16 +8,87 @@ namespace MySuperBank
 {
     public class BankAccount
     {
+        // Number represents Account Number. Defined as a String - can have letters in the account "number".
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+
+        // Updated Balance to set balance to 0 and foreach new transaction at to the previous balance, then return the balance.
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        // Private, can only be accessed by the BankAccount Class
+        private static int accountNumberSeed = 1234567890;
+
+        // Created new List of Transactions
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        // Bank Account Object - Template for Bank Accounts
+        public BankAccount(string name, decimal initialBalance)
+        {
+            // The Constructor - Initialize objects of the class BankAccount. "this" is not required.
+            this.Owner = name;
+            // Removed unnecessary initialBalance; Balance set to 0 on initialization of new account.
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+
+            // Setting the BankAccount account Number to start at the seed and then increment for each new account number.
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            // Error Handling
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            // Error Handling
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        // Create an account history
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            // HEADER
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                // ROWS
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
         }
     }
 }
--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -6,7 +6,44 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a Hardcoded new Object
+            var account = new BankAccount("Kendra", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            // Hardcode a Withdrawls as a Test. Extra Console.WriteLines unnecessary after creation of GetAccountHistory method.
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            //Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            //Console.WriteLine(account.Balance);
+
+            // Hardcode Deposit as a Test
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            //Console.WriteLine(account.Balance);
+
+            // Added Report for Account History
+            Console.WriteLine(account.GetAccountHistory());
+
+            ////Test that the initial balances must be positive.
+            //try
+            //{
+            //    var invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //}
+
+            ////Test for a negative balance.
+            //try
+            //    {
+            //        account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //    }
+            //    catch (InvalidOperationException e)
+            //    {
+            //        Console.WriteLine("Exception caught trying to overdraw");
+            //        Console.WriteLine(e.ToString());
+            //    }
         }
     }
 }
